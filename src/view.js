5232dde8918db952ff1a03dfc281f15333058560
Sulu Fiti <email": null>
2015-11-09 17:14:25 +1300

    style show post title, post elements

--- src/view.js
+++ src/view.js
@@ -1,13 +1,11 @@
  var View = (function(){
   function View(){}
   View.prototype.render = function(postData){
-    console.log(postData)
-    $("#posts").append("<div class='post'><h4>" +
-        postData.title +
-      "<h4><div class='post'>" +
-        postData.post +
+    $("#posts").append("<div class='post'><h4>"+
+        postData.title+
+      "<h4><div class='post'>"+
+        postData.post+
       "</div></div>")
   }
-
   return View
 })()
